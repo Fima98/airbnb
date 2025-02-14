4b239034bc458f35ac2acd8ff52a2e053fd82048
Fima <loboda.fima@icloud.com>
2025-02-14 17:05:45 +0200

    initial commit

---
+++ app/lib/actions.ts
@@ -0,0 +1,29 @@
+'use server';
+
+import { cookies } from "next/headers";
+
+export async function handleLogin(userId: string, accessToken: string, refreshToken: string) {
+    const cookie = await cookies();
+
+
+    cookie.set('session_userid', userId, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 60 * 60 * 24 * 7,
+        path: '/',
+    });
+
+    cookie.set('session_access_token', accessToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 60 * 60,
+        path: '/',
+    });
+
+    cookie.set('session_refresh_token', refreshToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 60 * 60 * 24 * 7,
+        path: '/',
+    });
+}
