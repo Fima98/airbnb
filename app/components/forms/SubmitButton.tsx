48fbb8a9f71e1d69146f7df214ba2f0ac5b27b9c
Fima <loboda.fima@icloud.com>
2025-02-20 10:50:30 +0200

    first commit

---
+++ app/components/forms/SubmitButton.tsx
@@ -0,0 +1,25 @@
+interface SubmitButtonProps {
+    label: string;
+    onClick?: () => void;
+    className?: string;
+}
+
+const SubmitButton: React.FC<SubmitButtonProps> = ({
+    label,
+    onClick = () => {},
+    className,
+}) => {
+    return (
+        <button
+            type="submit"
+            onClick={onClick}
+            className={`py-4 px-6 bg-airbnb hover:bg-airbnb-dark text-white rounded-xl transition cursor-pointer ${
+                className || ""
+            }`}
+        >
+            {label}
+        </button>
+    );
+};
+
+export default SubmitButton;
